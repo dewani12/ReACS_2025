9ab147b01cc95a1f5b1ebe9a703701cdc968e910
@dewani12 <gauravdewani2@gmail.com>
2026-03-12 13:55:35 +0530

    update: add link to ReACS 2025 Conference Proceedings in MainSection

--- frontend/src/components/MainSection.jsx
+++ frontend/src/components/MainSection.jsx
@@ -81,6 +81,10 @@ function MainSection() {
             </div>
 
             <div className="mb-6 pb-4 border-b-1">
+                <a href="https://ieeexplore.ieee.org/xpl/conhome/11412347/proceeding" className="text-blue-600 underline font-semibold">The ReACS 2025 Conference Proceedings are now available on IEEEXplore</a>
+            </div>
+
+            <div className="mb-6 pb-4 border-b-1">
                 <div className="heading-cl text-3xl md:text-4xl tracking-normal font-bold">Welcome to ReACS 2025</div>
                 <div className="text-sm/relaxed mt-4 tracking-wide text-justify"><span className="font-semibold">Welcome to the 2025 IEEE International Conference on Recent Advances in Computing and Systems (ReACS2025) is being organised by the Department of CSE, ABV-IIITM</span>, to functionalize it as a platform for dissemination of recent high-quality research work in the domain of Computing and Systems. This conference (<a href="https://conferences.ieee.org/conferences_events/conferences/conferencedetails/67479" className="text-blue-600 underline font-semibold">IEEE Conference ID: 67479</a>) covers the wide domain of Computer Science and Engineering and will provide a means to exchange innovative ideas and recent advances among researchers from academia and industry.</div>
             </div>
